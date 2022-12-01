a2ea9dd9670a93d4a48d28984b3bf7150dabee9a
cntruong7701 <cntruong12c5nct@gmail.com>
2022-12-01 14:50:49 +0700

    update

---
+++ sage/app/public/wp-content/themes/sage-theme/resources/views/template-parts/page/module-cao_nhat_truong.blade.php
@@ -0,0 +1,38 @@
+<section class="containers">
+    <main class="main-content">
+        <div class="lg:w-[1200px] lg:max-w-full md:text-center">
+            <div class="lg:flex lg:justify-center md:w-4/5 md:flex-none md:justify-center">
+                <div class="lg:w-1/2 md:w-full">
+                    <div class="Line-4"></div>
+                    <div class="w-full">
+                        <h2 class="text-5xl font-bold not-italic tracking-normal">The Encompass Platform Seamlessly Connects the Supply Chain, Allowing You to</h2>
+                    </div>
+                </div>
+                <div class="lg:w-1/2 md:w-full">
+                    <div class="w-full flex">
+                        <div class="lg:w-1/2 md:w-full md:text-center">
+                            <div class="w-full">
+                                <h2 class="Drive mb-6 text-xl font-semibold not-italic leading-[1.17] tracking-[-0.19px]">Drive Business Growth</h2>
+                                <p class="Drive-p w-4/5 text-[18px] font-light not-italic leading-[1.33] tracking-[-0.14px]">Tap into the power of predictive analytics and demand forecasting. Capitalize on revenue-generating opportunities faster. Capture mission-critical data across your entire operation—including all three supply chain tiers—empowering you to bring your beverage or food business into new levels of profitability. </p>
+                            </div>
+                            <div class="w-full">
+                                <h2 class="Improve mb-6 text-xl font-semibold not-italic leading-[1.17] tracking-[-0.19px]">Improve Operational Efficiencies</h2>
+                                <p class="Improve-p w-4/5 text-[18px] font-light not-italic leading-[1.33] tracking-[-0.14px]">Streamline processes and save money by reducing leakage with a fully integrated ERP system that works for your business. Encompass ERP makes it possible to identify where, when, and how resources can be best allocated to provide the most optimal outcome. </p>
+                            </div>
+                        </div>
+                        <div class="lg:w-1/2 md:w-full md:text-center">
+                            <div class="w-full">
+                                <h2 class="Leverage mb-6 text-xl font-semibold not-italic leading-[1.17] tracking-[-0.19px]">Leverage Real-Time Data for Better Decision-Making</h2>
+                                <p class="Leverage-p w-4/5 text-[18px] font-light not-italic leading-[1.33] tracking-[-0.14px]">Accelerate business decisions with better, more accurate system-to-system data and comprehensive reporting that results in deep, actionable insights. Stay ahead of potential disruptions and maintain a finger on the pulse of your supply chain from anywhere in the world. </p>
+                            </div>
+                            <div class="w-full">
+                                <h2 class="Enhance mb-6 text-xl font-semibold not-italic leading-[1.17] tracking-[-0.19px]">Enhance Team Dynamics & Employee Satisfaction</h2>
+                                <p class="Enhance-p w-4/5 text-[18px] font-light not-italic leading-[1.33] tracking-[-0.14px]">When your labor force has the tools they need to succeed, the workplace runs like a well-oiled machine. With Encompass ERP, every stakeholder across your supply chain—from purchasing to Point-of-Sale—has what they need to do their jobs effectively. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</section>
